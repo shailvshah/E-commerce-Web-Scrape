61fea48567e1fdc858ad6463969552c93c56fe1f
Shail Shah <shailvshah@gmail.com>
2020-02-27 01:34:13 -0600

    reviews logic added

--- BestBuy_Scraping.py
+++ BestBuy_Scraping.py
@@ -5,14 +5,11 @@ Created on Sun Feb 5 22:21:50 2020
 @author: shail
 """
 
-import numpy as np
 import pandas as pd
 from bs4 import BeautifulSoup  # HTML data structure
-import urllib.request
 import requests
 import re
 from datetime import datetime
-from collections import OrderedDict
 
 #get product descriptions
 def getTitles(soup):
@@ -120,6 +117,121 @@ def getProductLinks(soup, total_pages,page_url, header):
     return productlinks
 
 
+def getReviews(page_url,k,modelname,sid):
+    reviews = []
+    timestamp = []
+    rid = []
+    helpful = []
+    unhelpful = []
+    skuid = []    
+    header = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'}
+    html = requests.get(page_url, headers = header)                 
+    soup = BeautifulSoup(html.content, 'html.parser')
+    
+    print('\n\n\n Writing into reviews for product - ',k)
+    con = soup.findAll("div", {"class":"reviews-pagination col-xs-4 col-lg-3"})
+    for c in con:
+        init = c.text
+        tot_reviews = init.split(" ")[5]
+        tot_reviews = re.sub(r',','',str(tot_reviews))
+        tot_reviews = int(tot_reviews)
+        r_icount = re.findall(r'-\d+',c.text)
+        r_icount = re.findall(r'\d+',str(r_icount))
+        r_icount = int(*r_icount)
+    print('\nNo. of reviews in product '+str(k)+' are '+str(tot_reviews))
+    if tot_reviews<=r_icount:
+        r_tot_pages = 1
+    else:
+        if tot_reviews%r_icount == 0:
+            r_tot_pages = tot_reviews//r_icount
+        else:
+            r_tot_pages = (tot_reviews//r_icount)+1
+            
+    if r_tot_pages>1:
+        j=tot_reviews
+        for i in range(r_tot_pages):
+            
+            #first page is base url
+            
+            if i == 0:
+                #print('Page - '+str(i+1)+' of '+str(r_tot_pages))
+                con = soup.findAll("div", {"class":"ugc-review-body body-copy-lg"})
+                for c in con:
+                    reviews.append(c.text)
+                    rid.append(modelname+'-'+str(j))
+                    skuid.append(sid)
+                    j-=1
+                con = soup.findAll("div",{"class":"review-context"})
+                for c in con:
+                    timestamp.append(c.time['title'])
+                con = soup.findAll("button",{"class":"btn btn-outline btn-sm helpfulness-button no-margin-l"})
+                for c in con:
+                    dummy = c.text
+                    dummy = dummy.split(' ')[-1]
+                    helpful.append(int(re.search(r'\d+',str(dummy)).group()))
+                con = soup.findAll("button",{"class":"btn-default-link link neg-feedback"})
+                for c in con:
+                    dummy = c.text
+                    dummy = dummy.split(' ')[-1]
+                    unhelpful.append(int(re.search(r'\d+',str(dummy)).group()))
+            else:
+                #print('Page - '+str(i+1)+' of '+str(r_tot_pages))
+                page_url_review = page_url+'&page='+str(i+1)
+                html = requests.get(page_url_review, headers = header)
+                #create new soup object for new url and scrape
+                soup1 = BeautifulSoup(html.content, 'html.parser')
+                con = soup1.findAll("div", {"class":"ugc-review-body body-copy-lg"})
+                for c in con:
+                    reviews.append(c.text)
+                    rid.append(modelname+'-'+str(j))
+                    skuid.append(sid)
+                    j-=1
+                con = soup1.findAll("div",{"class":"review-context"})
+                for c in con:
+                    timestamp.append(c.time['title'])
+                con = soup1.findAll("button",{"class":"btn btn-outline btn-sm helpfulness-button no-margin-l"})
+                for c in con:
+                    dummy = c.text
+                    dummy = dummy.split(' ')[-1]
+                    helpful.append(int(re.search(r'\d+',str(dummy)).group()))
+                con = soup1.findAll("button",{"class":"btn-default-link link neg-feedback"})
+                for c in con:
+                    dummy = c.text
+                    dummy = dummy.split(' ')[-1]
+                    unhelpful.append(int(re.search(r'\d+',str(dummy)).group()))
+            i = i+1
+    else:
+        #print('Page - 1 of '+str(r_tot_pages))
+        con = soup.findAll("div", {"class":"ugc-review-body body-copy-lg"})
+        j=tot_reviews
+        for c in con:
+            reviews.append(c.text)
+            rid.append(modelname+'-'+str(j))
+            skuid.append(sid)
+            j-=1
+        con = soup.findAll("div",{"class":"review-context"})
+        for c in con:
+            timestamp.append(c.time['title'])
+        con = soup.findAll("button",{"class":"btn btn-outline btn-sm helpfulness-button no-margin-l"})
+        for c in con:
+            dummy = c.text
+            dummy = dummy.split(' ')[-1]
+            helpful.append(int(re.search(r'\d+',str(dummy)).group()))
+        con = soup.findAll("button",{"class":"btn-default-link link neg-feedback"})
+        for c in con:
+            dummy = c.text
+            dummy = dummy.split(' ')[-1]
+            unhelpful.append(int(re.search(r'\d+',str(dummy)).group()))
+    
+    print('\nGoing over next product....')
+    product_review = pd.DataFrame(list(zip(skuid, timestamp, rid,reviews,helpful,unhelpful)),
+                             columns =['SKUid','Datetime','RID','Review','Helpful','Unhelpful'])
+    return product_review
+    
+        
+        
+
+
 def doTransform(currentprice, actualprice, ratings_count):
     star5 = []
     star4 = []
@@ -187,7 +299,7 @@ if __name__ == "__main__":
     html = requests.get(page_url, headers = header, timeout=5 )                 #timeouts argument can be avoided!
         
     soup = BeautifulSoup(html.content, 'html.parser')
-    
+    print('Starting to Scrape base URL...')
     #calc. total no. of items per page
     con = soup.findAll("div", {"class":"left-side"})
     for c in con:
@@ -201,6 +313,7 @@ if __name__ == "__main__":
         icount = re.findall(r'\d+',c.text)
         icount = int(*icount)
     
+    print('Items found under product category - ',icount)
     #calc total no of pages.
     if icount<=pcount:
         total_pages = 1
@@ -210,8 +323,10 @@ if __name__ == "__main__":
             total_pages = icount//pcount
         else:
             total_pages = icount//pcount+1
-    
+    print('Total no. of pages to scrape - ',total_pages)
     #store product links
+    
+    print('\n\n\n################### Extracting Product Links #######################')
     productlinks = getProductLinks(soup,total_pages,page_url,header)            
     
  
@@ -223,19 +338,32 @@ if __name__ == "__main__":
     ratings_count = []
     todaydate = []
     #find all details per product one by one!
+    i=1
+    print('\n\n\n################# Scraping Individual Products Content ################### ')
     for product in productlinks:                                                
         page_url = 'https://www.bestbuy.com/'+str(product)
         html = requests.get(page_url, headers = header)
-        soup = BeautifulSoup(html.content, 'html.parser')
-        titles.append(getTitles(soup))
-        skuid.append(getSKUID(soup))
-        modelno.append(getModelNo(soup))
-        ratings_count.append(getRating_Count(soup))
-        currentprice.append(getCurrentPrice(soup))
-        actualprice.append(getOriginalPrice(soup))
+        soup1 = BeautifulSoup(html.content, 'html.parser')
+        titles.append(getTitles(soup1))
+        skuid.append(getSKUID(soup1))
+        modelno.append(getModelNo(soup1))
+        ratings_count.append(getRating_Count(soup1))
+        currentprice.append(getCurrentPrice(soup1))
+        actualprice.append(getOriginalPrice(soup1))
         todaydate.append(datetime.date(datetime.now()))
+        if ratings_count[i-1] == '0.0':
+            print('No Reviews Found!')
+            productreview = pd.DataFrame(columns =['SKUid','Datetime','RID','Review','Helpful','Unhelpful'])
+            productreview.to_excel("reviews_"+skuid[i-1]+str(datetime.date(datetime.now()))+".xlsx") 
+        else:
+            page_url_review = product[:5]+'/reviews/'+product[6:]
+            page_url_review = page_url_review.split('.')[0]
+            page_url_review = 'https://www.bestbuy.com/'+str(page_url_review)+'?variant=A&sort=MOST_RECENT'
+            productreview = getReviews(page_url_review,i,modelno[i-1],skuid[i-1])
+            productreview.to_excel("reviews_"+skuid[i-1]+str(datetime.date(datetime.now()))+".xlsx") 
+        i+=1
         
-                    
+        
     #transform the result from above to further push into data frame for future manipulations!
     currentprice,actualprice,star5,star4,star3,star2,star1 = doTransform(currentprice,actualprice,ratings_count)
  
@@ -246,103 +374,14 @@ if __name__ == "__main__":
     product_info = pd.DataFrame(list(zip(skuid,todaydate,star1,star2,star3,star4,star5,currentprice,actualprice)),
                                 columns = ['SKUid','Date','1 star','2 stars','3 stars','4 stars','5 stars','Current Price','Original Price'])
     
+    product_description.to_excel("product_description_"+str(datetime.date(datetime.now()))+".xlsx")
+    
+    product_info.to_excel("product_info_"+str(datetime.date(datetime.now()))+".xlsx")
     
     
+
     
-    #WIP - reviews table 
     
-    reviews = []
-    timestamp = []
-    rname = []
-    rid = []
-    helpful = []
-    unhelpful = []
-    page_url_review = 'https://www.bestbuy.com/site/reviews/sony-65-class-led-x850g-series-2160p-smart-4k-uhd-tv-with-hdr/6356395?variant=A&sort=MOST_RECENT'
-    html = requests.get(page_url_review, headers = header)
-    soup = BeautifulSoup(html.content, 'html.parser')
-    
-    con = soup.findAll("div", {"class":"reviews-pagination col-xs-4 col-lg-3"})
-    for c in con:
-        init = c.text
-        tot_reviews = init.split(" ")[5]
-        tot_reviews = re.sub(r',','',str(tot_reviews))
-        tot_reviews = int(tot_reviews)
-        r_icount = re.findall(r'-\d+',c.text)
-        r_icount = re.findall(r'\d+',str(r_icount))
-        r_icount = int(*r_icount)
-    if tot_reviews<=r_icount:
-        r_tot_pages = 1
-    else:
-        if tot_reviews%r_icount == 0:
-            r_tot_pages = tot_reviews//r_icount
-        else:
-            r_tot_pages = (tot_reviews//r_icount)+1
-            
-    if r_tot_pages>1:
-        timestamp = []
-        reviews = []
-        helpful = []
-        unhelpful = []
-        for i in range(r_tot_pages):
-            i = i+1
-            #first page is base url
-            if i == 1:
-                con = soup.findAll("div", {"class":"ugc-review-body body-copy-lg"})
-                for c in con:
-                    reviews.append(c.text)
-                con = soup.findAll("div",{"class":"review-context"})
-                for c in con:
-                    timestamp.append(c.time['title'])
-                con = soup.findAll("button",{"class":"btn btn-outline btn-sm helpfulness-button no-margin-l"})
-                for c in con:
-                    dummy = c.text
-                    dummy = dummy.split(' ')[-1]
-                    helpful.append(int(re.search(r'\d+',str(dummy)).group()))
-                con = soup.findAll("button",{"class":"btn-default-link link neg-feedback"})
-                for c in con:
-                    dummy = c.text
-                    dummy = dummy.split(' ')[-1]
-                    unhelpful.append(int(re.search(r'\d+',str(dummy)).group()))
-            else:
-                page_url_review = 'https://www.bestbuy.com/site/reviews/sony-65-class-led-x850g-series-2160p-smart-4k-uhd-tv-with-hdr/6356395?variant=A&sort=MOST_RECENT&page='+str(i)
-                html = requests.get(page_url_review, headers = header, timeout=5 )
-                #create new soup object for new url and scrape
-                soup = BeautifulSoup(html.content, 'html.parser')
-                con = soup.findAll("div", {"class":"ugc-review-body body-copy-lg"})
-                for c in con:
-                    reviews.append(c.text)
-                con = soup.findAll("div",{"class":"review-context"})
-                for c in con:
-                    timestamp.append(c.time['title'])
-                con = soup.findAll("button",{"class":"btn btn-outline btn-sm helpfulness-button no-margin-l"})
-                for c in con:
-                    dummy = c.text
-                    dummy = dummy.split(' ')[-1]
-                    helpful.append(int(re.search(r'\d+',str(dummy)).group()))
-                con = soup.findAll("button",{"class":"btn-default-link link neg-feedback"})
-                for c in con:
-                    dummy = c.text
-                    dummy = dummy.split(' ')[-1]
-                    unhelpful.append(int(re.search(r'\d+',str(dummy)).group()))
-    else:
-        con = soup.findAll("div", {"class":"ugc-review-body body-copy-lg"})
-        for c in con:
-            reviews.append(c.text)
-        con = soup.findAll("div",{"class":"review-context"})
-        for c in con:
-            timestamp.append(c.time['title'])
-        con = soup.findAll("button",{"class":"btn btn-outline btn-sm helpfulness-button no-margin-l"})
-        for c in con:
-            dummy = c.text
-            dummy = dummy.split(' ')[-1]
-            helpful.append(int(re.search(r'\d+',str(dummy)).group()))
-        con = soup.findAll("button",{"class":"btn-default-link link neg-feedback"})
-        for c in con:
-            dummy = c.text
-            dummy = dummy.split(' ')[-1]
-            unhelpful.append(int(re.search(r'\d+',str(dummy)).group()))
-        
-        
     
   
         
