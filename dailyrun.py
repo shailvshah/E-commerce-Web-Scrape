a9bf21d258d68a6f3fa15de73b7fbcd1b763133d
Shail Shah <shailvshah@gmail.com>
2020-03-06 15:21:06 -0600

    dailyrun v.1

--- dailyrun.py
+++ dailyrun.py
@@ -11,5 +11,109 @@ from bs4 import BeautifulSoup  # HTML data structure
 import requests
 import re
 from datetime import datetime
+from BestBuy_Scraping import getProductLinks,getTitles,getSKUID,getModelNo,getCurrentPrice,getOriginalPrice,getRating_Count,doTransform
 
-data = pd.read_excel('product_reviews.xlsx')
+
+
+
+
+
+if __name__ == "__main__":
+    page_url = 'https://www.bestbuy.com/site/tvs/65-inch-tvs/pcmcat1514910447059.c?id=pcmcat1514910447059'
+    header = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'}
+    html = requests.get(page_url, headers = header, timeout=5 )                 #timeouts argument can be avoided!
+        
+    soup = BeautifulSoup(html.content, 'html.parser')
+    print('Starting to Scrape base URL...')
+    
+    #calc. total no. of items per page
+    con = soup.findAll("div", {"class":"left-side"})
+    for c in con:
+        pcount = re.findall(r'-\d+',c.text)
+        pcount = re.findall(r'\d+',str(pcount))
+        pcount = int(*pcount)
+    
+    #calc total items for product category
+    conn = soup.findAll("div", {"class":"banner-middle-column"})
+    for c in conn:
+        icount = re.findall(r'\d+',c.text)
+        icount = int(*icount)
+    
+    print('Items found under product category - ',icount)
+    #calc total no of pages.
+    if icount<=pcount:
+        total_pages = 1
+    else:    
+    
+        if icount%pcount==0:
+            total_pages = icount//pcount
+        else:
+            total_pages = icount//pcount+1
+    print('Total no. of pages to scrape - ',total_pages)
+    #store product links
+    
+    print('\n\n\n################### Extracting Product Links #######################')
+    productlinks = getProductLinks(soup,total_pages,page_url,header)            
+    
+ 
+    titles = []
+    skuid = []
+    modelno = []
+    currentprice = []
+    actualprice = []
+    ratings_count = []
+    todaydate = []
+    #find all details per product one by one!
+    i=1
+    print('\n\n\n################# Scraping Individual Products Content ################### ')
+    for product in productlinks:                                                
+        page_url = 'https://www.bestbuy.com/'+str(product)
+        html = requests.get(page_url, headers = header)
+        soup1 = BeautifulSoup(html.content, 'html.parser')
+        titles.append(getTitles(soup1))
+        skuid.append(getSKUID(soup1))
+        modelno.append(getModelNo(soup1))
+        ratings_count.append(getRating_Count(soup1))
+        currentprice.append(getCurrentPrice(soup1))
+        actualprice.append(getOriginalPrice(soup1))
+        todaydate.append(datetime.date(datetime.now()))
+        
+    #transform the result from above to further push into data frame for future manipulations!
+    currentprice,actualprice,star5,star4,star3,star2,star1 = doTransform(currentprice,actualprice,ratings_count)
+ 
+   
+    #create dataframe
+    product_description = pd.DataFrame(list(zip(skuid, titles, modelno)),
+                             columns =['SKUid','Item Descrption','ModelNo'])
+    product_info = pd.DataFrame(list(zip(skuid,todaydate,star1,star2,star3,star4,star5,currentprice,actualprice)),
+                                columns = ['SKUid','Date','1 star','2 stars','3 stars','4 stars','5 stars','Current Price','Original Price'])
+    
+    product_description.to_excel("product_description.xlsx")
+    with pd.ExcelWriter('product_info.xlsx',mode='a') as writer:  
+        product_info.to_excel(writer)
+        
+        
+"""     WIP - Reviews format and manipulation still lil tricky... 
+    i=1
+    print('\n\n\n################# Scraping and appending reviews  ################### ')
+    for product in productlinks:                                                
+        page_url = 'https://www.bestbuy.com/'+str(product)
+        html = requests.get(page_url, headers = header)
+        soup1 = BeautifulSoup(html.content, 'html.parser')
+        titles.append(getTitles(soup1))
+        skuid.append(getSKUID(soup1))
+        modelno.append(getModelNo(soup1))
+        ratings_count.append(getRating_Count(soup1))
+        if ratings_count[i-1] != 0:
+            page_url_review = product[:5]+'/reviews/'+product[6:]
+            page_url_review = page_url_review.split('.')[0]
+            page_url_review = 'https://www.bestbuy.com/'+str(page_url_review)
+            productreview = getReviews(page_url_review,i,modelno[i-1],skuid[i-1])
+            with pd.ExcelWriter('reviews.xlsx',mode='a') as writer:  
+                product_info.to_excel(writer) 
+        else:
+            continue 
+        i+=1
+    
+"""    
+    
--- reviews_manipulation.py
+++ reviews_manipulation.py
@@ -6,24 +6,18 @@ Created on Mon Mar  2 18:39:36 2020
 """
 
 import pandas as pd
-import uuid
 
-data_reviews = pd.read_excel('product_reviews.xlsx')
 
-data_reviews['Datetime'] = pd.to_datetime(data_reviews['Datetime'])
-
-data_reviews['Date'] = pd.to_datetime(data_reviews['Datetime']).dt.date
-
-data_reviews['Time'] = pd.to_datetime(data_reviews['Datetime']).dt.time
-
-data_reviews = data_reviews.sort_values(by=['Datetime'])
-
-data_reviews = data_reviews.reset_index(drop=True)
-
-data_reviews = data_reviews[['SKUid','Date','Time','Review','Helpful','Unhelpful','Datetime','RID']]
-
-data_reviews = data_reviews.drop(['Datetime','RID'],axis=1)
-
-data_reviews['Rid'] = [uuid.uuid4() for _ in range(len(data_reviews.index))]
-
-data_reviews.to_excel('reviews.xlsx',index=False)
+def review_manipulation():
+    data_reviews = pd.read_excel('product_reviews.xlsx')
+    data_reviews['Datetime'] = pd.to_datetime(data_reviews['Datetime'])
+    data_reviews['Date'] = pd.to_datetime(data_reviews['Datetime']).dt.date
+    data_reviews['Time'] = pd.to_datetime(data_reviews['Datetime']).dt.time
+    data_reviews['RID'] = data_reviews['RID'].str.split('-').str[-1]
+    data_reviews = data_reviews.sort_values(by=['Datetime'])
+    data_reviews = data_reviews.drop(['Datetime'],axis=1)
+    data_reviews = data_reviews.reset_index(drop=True)
+    
+    data_reviews = data_reviews[['SKUid','Date','Time','Review','Helpful','Unhelpful','RID']]
+    
+    return data_reviews
